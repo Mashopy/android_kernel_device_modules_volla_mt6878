c7441fa50c15ff67d76a5c2bb37cadbe50bff319
Lucas Tsai <cpi00079@mediatek.com>
2025-03-31 02:08:29 +0800

    [ALPS09866083] tcpc: update the tcpc driver to v2.0.31_MTK
    [Description]
    (1) Do I2C/IO transactions when system resumed
    (2) Reduce log printing
    (3) Revise attach/detach conditions/actions
    (4) Disable FOD
    (5) Add support for RT1718S
    (6) Replace 64-bit divisions with do_div() calls
    (7) Disable Rx SOP when in ready states
    (8) Revise BIST flows
    (9) Revise discharge controls
    (10) Control CC Open in the deinit ops
    (11) Revise sink_vbus of standby current
    (12) Implement alert ratelimit mechanism
    (13) Disable CONFIG_USB_PD_DISCARD_AND_UNEXPECT_MSG
    (14) Revise cable discovery flow
    (15) Separate tSenderResponse for PD2 and PD3
    (16) Enter low power mode with 5ms delay after unattached
    (17) Fix tcpm_bk sync issues
    (18) Fix SinkTxNG
    (19) Call pm_system_wakeup() in delayed_work handler function
    (20) Update pd_transmit_state when PD Hard Reset failed
    (21) Remove the unwanted PD Hard Reset in tcpm.c
    (22) Revise the feature of VBUS shorted to CC
    (23) Handle typec timers first
    (24) Revise the logics of expected_svid
    (25) Replace BUG_ON with WARN_ON
    (26) Start tPDDebounce always when CC Open at Attached.SNK
    (27) Let CC pins re-toggle after entering lpm
    (28) Add ps_changed flow
    (29) Revise Rx flow
    (30) Reset pd_wait_pr_swap_complete at receiving Not_Supported
    
    [Test]
    LeCroy and Ellisys Compliance Test
    
    MTK-Commit-Id: 3206e619eb4caf8523dd6eda989fba9c97614ee8
    
    CR-Id: ALPS09866083
    Signed-off-by: Lucas Tsai <cpi00079@mediatek.com>
    Feature: rtk_driver
    Change-Id: I217320701426ec79a2b55dc2571b9b10168d8fdb

--- drivers/misc/mediatek/typec/tcpc/rt_pd_manager.c
+++ drivers/misc/mediatek/typec/tcpc/rt_pd_manager.c
@@ -3,8 +3,9 @@
  * Copyright (c) 2021 MediaTek Inc.
  */
 
-#include <linux/kernel.h>
 #include <linux/module.h>
+#if IS_ENABLED(CONFIG_TCPC_CLASS)
+#include <linux/kernel.h>
 #include <linux/of.h>
 #include <linux/platform_device.h>
 #include <linux/power_supply.h>
@@ -15,6 +16,14 @@
 #include "inc/tcpci_typec.h"
 
 #define RT_PD_MANAGER_VERSION	"1.0.11"
+
+static bool dbg_log_en = true;
+module_param(dbg_log_en, bool, 0644);
+#define mt_dbg(dev, fmt, ...) \
+	do { \
+		if (dbg_log_en) \
+			dev_info(dev, "%s " fmt, __func__, ##__VA_ARGS__); \
+	} while(0)
 
 struct rpmd_notifier_block {
 	struct notifier_block nb;
@@ -43,6 +52,7 @@ static int pd_tcp_notifier_call(struct notifier_block *nb,
 		container_of(nb, struct rpmd_notifier_block, nb);
 	struct rt_pd_manager_data *rpmd = pd_nb->rpmd;
 	int ret = 0, idx = pd_nb - rpmd->pd_nb;
+	int bootmode = rpmd->tcpc[idx]->bootmode;
 	uint8_t old_state = TYPEC_UNATTACHED, new_state = TYPEC_UNATTACHED;
 	enum typec_pwr_opmode opmode = TYPEC_PWR_MODE_USB;
 	uint16_t pd_revision = 0x0316;
@@ -50,17 +60,16 @@ static int pd_tcp_notifier_call(struct notifier_block *nb,
 	struct typec_displayport_data dp_data = {.status = 0, .conf = 0};
 	struct typec_mux_state state = {.mode = 0, .data = &dp_data};
 
-	dev_info(rpmd->dev, "%s event = %lu, idx = %d\n", __func__, event, idx);
-
+	mt_dbg(rpmd->dev, "event = %lu, idx = %d\n", event, idx);
 	switch (event) {
 	case TCP_NOTIFY_VBUS_SHORT_CC:
-		if (noti->vsc_status.short_status)
-			dev_info(rpmd->dev,
-				 "%s enter short status, short_cc = %s\n",
-				 __func__, noti->vsc_status.short_cc ==
-				 TCPC_POLARITY_CC1 ? "CC1" : "CC2");
-		else
+		if (!noti->vsc_status) {
 			dev_info(rpmd->dev, "%s exit short status\n", __func__);
+			break;
+		}
+		dev_info(rpmd->dev, "%s enter short status, CC%s%s\n", __func__,
+			 noti->vsc_status & BIT(TCPC_POLARITY_CC1) ? "1" : "",
+			 noti->vsc_status & BIT(TCPC_POLARITY_CC2) ? "2" : "");
 		break;
 	case TCP_NOTIFY_SINK_VBUS:
 		dev_info(rpmd->dev, "%s sink vbus %dmV %dmA type(0x%02X)\n",
@@ -156,7 +165,8 @@ static int pd_tcp_notifier_call(struct notifier_block *nb,
 			/* disable AudioAccessory connection */
 		}
 
-		if (new_state == TYPEC_UNATTACHED) {
+		if (new_state == TYPEC_UNATTACHED ||
+		    new_state == TYPEC_PROTECTION) {
 			typec_unregister_partner(rpmd->partner[idx]);
 			rpmd->partner[idx] = NULL;
 			if (rpmd->typec_caps[idx].prefer_role == TYPEC_SOURCE) {
@@ -337,6 +347,8 @@ static int pd_tcp_notifier_call(struct notifier_block *nb,
 		typec_mux_set(rpmd->mux[idx], &state);
 		break;
 	case TCP_NOTIFY_WD0_STATE:
+		if (bootmode == 8 || bootmode == 9)
+			break;
 		tcpm_typec_change_role_postpone(rpmd->tcpc[idx],
 						noti->wd0_state.wd0 ?
 						rpmd->role_def[idx] :
@@ -348,6 +360,10 @@ static int pd_tcp_notifier_call(struct notifier_block *nb,
 		break;
 	case TCP_NOTIFY_CC_HI:
 		dev_info(rpmd->dev, "%s cc_hi = %d\n", __func__, noti->cc_hi);
+		break;
+	case TCP_NOTIFY_ALERT_RATELIMITED:
+		dev_info(rpmd->dev, "%s alert_ratelimited = %d\n",
+				    __func__, noti->alert_ratelimited);
 		break;
 	default:
 		break;
@@ -718,7 +734,7 @@ MODULE_DEVICE_TABLE(of, rt_pd_manager_of_match);
 static struct platform_driver rt_pd_manager_driver = {
 	.driver = {
 		.name = "rt-pd-manager",
-		.of_match_table = of_match_ptr(rt_pd_manager_of_match),
+		.of_match_table = rt_pd_manager_of_match,
 	},
 	.probe = rt_pd_manager_probe,
 	.remove = rt_pd_manager_remove,
@@ -738,8 +754,9 @@ module_exit(rt_pd_manager_exit);
 
 MODULE_AUTHOR("Jeff Chang");
 MODULE_DESCRIPTION("Richtek pd manager driver");
-MODULE_LICENSE("GPL");
 MODULE_VERSION(RT_PD_MANAGER_VERSION);
+#endif	/* CONFIG_TCPC_CLASS */
+MODULE_LICENSE("GPL");
 
 /*
  * Release Note
